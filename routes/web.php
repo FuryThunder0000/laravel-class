af6d700682cc9cb250f320ec5067a43881c0713d
anasfalah3 <anasfalah3@gmail.com>
2024-05-16 10:21:34 +0100

    middleware part 1

--- routes/web.php
+++ routes/web.php
@@ -13,56 +13,69 @@ Route::get('/', function () {
     return view('layout');
 });
 
-Route::resource('filieres',FiliereController::class);
-Route::resource('groupes',GroupeController::class);
-Route::resource('stagiaires',StagiaireController::class);
-Route::resource('formateurs',FormateurController::class);
+Route::resource('filieres', FiliereController::class);
+Route::resource('groupes', GroupeController::class);
+Route::resource('stagiaires', StagiaireController::class);
+Route::resource('formateurs', FormateurController::class);
 
-Route::get('/formateursGroupes',[FormateurGroupeController::class,'index'])->name('formateursGroupes.index');
-Route::get('/formateursGroupes/create',[FormateurGroupeController::class,'create'])->name('formateursGroupes.create');
-Route::post('/formateursGroupes/affecter',[FormateurGroupeController::class,'affecter'])->name('formateursGroupes.affecter');
-Route::delete('/formateursGroupes/{formateur_id}/{groupe_id}',[FormateurGroupeController::class,'destroy'])->name('formateursGroupes.destroy');
-Route::get('/formateursGroupes/{id}/edit',[FormateurGroupeController::class,'edit'])->name('formateursGroupes.edit');
-Route::put('/formateursGroupes/update',[FormateurGroupeController::class,'update'])->name('formateursGroupes.update');
+Route::get('/formateursGroupes', [FormateurGroupeController::class, 'index'])->name('formateursGroupes.index');
+Route::get('/formateursGroupes/create', [FormateurGroupeController::class, 'create'])->name('formateursGroupes.create');
+Route::post('/formateursGroupes/affecter', [FormateurGroupeController::class, 'affecter'])->name('formateursGroupes.affecter');
+Route::delete('/formateursGroupes/{formateur_id}/{groupe_id}', [FormateurGroupeController::class, 'destroy'])->name('formateursGroupes.destroy');
+Route::get('/formateursGroupes/{id}/edit', [FormateurGroupeController::class, 'edit'])->name('formateursGroupes.edit');
+Route::put('/formateursGroupes/update', [FormateurGroupeController::class, 'update'])->name('formateursGroupes.update');
 
 Route::get('/Recherche', function () {
     return view('recherche.index');
 })->name('Recherche');
 
-Route::get('/Recherche/RechercheGroupeStagiairesAnneeFormation',
-            [RechercheController::class , 'RechercheGroupeStagiairesAnneeFormation']
-        )->name('Recherche.RechercheGroupeStagiairesAnneeFormation');
+Route::get(
+    '/Recherche/RechercheGroupeStagiairesAnneeFormation',
+    [RechercheController::class, 'RechercheGroupeStagiairesAnneeFormation']
+)->name('Recherche.RechercheGroupeStagiairesAnneeFormation');
 
-Route::get('/Recherche/RechercheStagiairesParGroupe',
-            [RechercheController::class , 'RechercheGroupe']
-        )->name('Recherche.RechercheGroupe');
+Route::get(
+    '/Recherche/RechercheStagiairesParGroupe',
+    [RechercheController::class, 'RechercheGroupe']
+)->name('Recherche.RechercheGroupe');
 
-Route::post('/Recherche/RechercheStagiairesParGroupe',
-            [RechercheController::class , 'RechercheStagiairesParGroupe']
-        )->name('Recherche.RechercheStagiairesParGroupe');
+Route::post(
+    '/Recherche/RechercheStagiairesParGroupe',
+    [RechercheController::class, 'RechercheStagiairesParGroupe']
+)->name('Recherche.RechercheStagiairesParGroupe');
 
-Route::get('/Recherche/RechercheFormateurParMatricule',
-            [RechercheController::class , 'RechercheFormateur']
-        )->name('Recherche.RechercheFormateur');
+Route::get(
+    '/Recherche/RechercheFormateurParMatricule',
+    [RechercheController::class, 'RechercheFormateur']
+)->name('Recherche.RechercheFormateur');
 
-Route::post('/Recherche/RechercheFormateurParMatricule',
-            [RechercheController::class , 'RechercheFormateurParMatricule']
-        )->name('Recherche.RechercheFormateurParMatricule');
+Route::post(
+    '/Recherche/RechercheFormateurParMatricule',
+    [RechercheController::class, 'RechercheFormateurParMatricule']
+)->name('Recherche.RechercheFormateurParMatricule');
 
-Route::get('/Recherche/RechercheNbStagiaireParGroupe',
-        [RechercheController::class , 'RechercheNbStagiaireParGroupe']
-        )->name('Recherche.RechercheNbStagiaireParGroupe');
-        
-Route::get('/Recherche/RechercheFormateurMinMaxSalaire',
-        [RechercheController::class , 'RechercheFormateurMinMaxSalaire']
-        )->name('Recherche.RechercheFormateurMinMaxSalaire');
+Route::get(
+    '/Recherche/RechercheNbStagiaireParGroupe',
+    [RechercheController::class, 'RechercheNbStagiaireParGroupe']
+)->name('Recherche.RechercheNbStagiaireParGroupe');
 
-Route::get('/Recherche/RechercheFormateurParSalaire',
-        [RechercheController::class , 'RechercheFormateurParSalaires']
-    )->name('Recherche.RechercheFormateurParSalaire');
+Route::get(
+    '/Recherche/RechercheFormateurMinMaxSalaire',
+    [RechercheController::class, 'RechercheFormateurMinMaxSalaire']
+)->name('Recherche.RechercheFormateurMinMaxSalaire');
 
-Route::post('/Recherche/RechercheFormateurTranchSalaire',
-        [RechercheController::class , 'RechercheFormateurTranchSalaire']
-    )->name('Recherche.RechercheFormateurTranchSalaire');
+Route::get(
+    '/Recherche/RechercheFormateurParSalaire',
+    [RechercheController::class, 'RechercheFormateurParSalaires']
+)->name('Recherche.RechercheFormateurParSalaire');
+
+Route::post(
+    '/Recherche/RechercheFormateurTranchSalaire',
+    [RechercheController::class, 'RechercheFormateurTranchSalaire']
+)->name('Recherche.RechercheFormateurTranchSalaire');
 
 
+Route::view('/home', 'middlewareTP.home');
+Route::post('/page_secret', function () {
+    return view('middlewareTP.page_secret');
+})->name('page_secret')->middleware('checkAge');
